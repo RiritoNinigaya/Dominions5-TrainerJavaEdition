7260c663b7f8ecca9f1be3657b3841a4752fd427
Ririto Ninigaya <riritoninigaya@hotmail.com>
2024-05-04 15:24:21 +0300

    Update Main.java

--- Dominions5Trainer/src/Main.java
+++ Dominions5Trainer/src/Main.java
@@ -1,21 +1,32 @@
+//
+// Source code recreated from a .class file by IntelliJ IDEA
+// (powered by FernFlower decompiler)
+//
+
 import imgui.ImGui;
 import imgui.app.Application;
 import imgui.app.Configuration;
-public class Main extends Application
-{
-    @Override
+
+public class Main extends Application {
+    public Main() {
+    }
+
     protected void configure(Configuration config) {
         config.setTitle("Dominions 5 Trainer by RiritoNinigaya");
     }
 
-    @Override
     public void process() {
         ImGui.begin("Dominions5 Trainer by RiritoNinigaya");
-        ImGui.text("Hello, World!");
-        ImGui.setWindowSize(155, 300);
+        ImGui.text("Hi, this is My First Trainer for Dominions 5... \nSo Enjoy to use this!!!");
+        ImGui.setWindowSize(455.0F, 455.0F);
+        if (ImGui.button("Unlimited Perls in Player 1")) {
+            System.out.printf("\nComing Soon!!!");
+        }
+
         ImGui.end();
         ImGui.render();
     }
+
     public static void main(String[] args) {
         System.out.printf("Welcome To Dominions5 Trainer by RiritoNinigaya");
         launch(new Main());
